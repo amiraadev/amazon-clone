737f73db52292b7993a190a6e6f5f57d91fd4250
amirticha <allagui_fawzi@outlook.fr>
2023-08-19 14:45:26 +0100

    second commit

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Allaguiamira:A12648659a@cluster0.uxy6jat.mongodb.net/amazon-clone',
+    ),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/product/product.controller.ts
+++ api/src/product/product.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  @Post()
+  createPost(
+    @Body('name') name: string,
+    @Body('price') price: string,
+    @Body('description') description?: string,
+  ) {
+    return this.productService.create(name, price, description);
+  }
+}
